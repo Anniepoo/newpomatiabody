0eb0eb0e7425a1523a48b9f0ecec42d4b60481f9
Anne Ogborn <annie66us@yahoo.com>
2021-03-29 18:03:41 +0200

    move parts, add calculations, add optoencoders to wheels

--- calculations.pl
+++ calculations.pl
@@ -18,19 +18,20 @@ engineer(X) :-
         LevelSpeed,  % min top level speed  mm/sec
         AvailTractiveEffort
         ],
-    MassWOBattery = 12.0,
+    MassWOBattery = 12.0,  % mass of the robot without the battery installed
     { TotalMass =:= BatteryMass + MassWOBattery },
-    G = 9.8,
-    CoeffRollingFric = 2.5,  % mm
+    G = 9.8,   % acceleration due to gravity
+    CoeffRollingFric = 2.5,  % mm   converted from m value given on https://www.engineeringtoolbox.com/rolling-friction-resistance-d_1303.html
     { Weight =:= G * TotalMass },
     WheelRad = 112.0, % mm
     { RollingResistance =:= Weight * CoeffRollingFric / WheelRad },
-    RulingGrade = 0.1,
+    RulingGrade = 0.1,   % the rise/run of the steepest grade we want to ascend at any speed
     { LiftResistance =:= RulingGrade * Weight },
     { Resistance =:= LiftResistance + RollingResistance },
     StallTorque = 400.0,  % Nt-mm
-    LevelSpeed = 2000.0, % mm/sec
+    LevelSpeed = 2000.0, % mm/sec     design criteria speed on level ground
  %   LevelSpeed is RollerRadius * 2.0 * pi * 4000.0 / 200.0,
+ % the roller must be at least this diameter to acheive the design speed
     RollerRadius is LevelSpeed / 2.0 / pi / 4000.0 * 200.0,
     { AvailTractiveEffort =:= 2.0 * StallTorque / RollerRadius }, % two motors!
     BatteryMass  = 10.0.
